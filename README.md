f45deabaf12acb4aa79aee4adc676ace0312cb5d
JuxGD <jak@e.email>
2024-11-07 18:54:58 -0300

    ok NOW it should ABSOLUTELY work

---
+++ README.md
@@ -0,0 +1,3 @@
+# JuxGD
+
+Hi, I'm Jux! A Geometry Dash player and aspiring YouTuber, music producer, and developer.
---
+++ components/Page.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="container">
+    <h1>🌈Jux's awesome little personal page</h1>
+    Hi, I'm Jux and this is my awesome little personal page!
+    <br />
+    <br />
+    <h3>About me</h3>
+    <img
+      width="100vw"
+      src="./nb.svg"
+      alt="Nonbinary flag"
+    /><br />
+    <img
+      width="100vw"
+      src="./aro.svg"
+      alt="Aromantic flag"
+    /><br />
+    I'm a Geometry Dash player and aspiring GD YouTuber, music producer and
+    software developer (mostly games, mods and useful tools/websites).<br />
+    <ul>
+      <li>Non-binary (any pronouns)</li>
+      <li>16</li>
+      <li>Hardest GD level is Jawbreaker by ZenthicAlpha</li>
+      <li>Queer, plural and alterhuman friendly</li>
+    </ul>
+
+    <h3>Links</h3>
+    <ul>
+      <li>
+        My <a rel="me" href="https://github.com/JuxGD">GitHub</a>, full of abandoned
+        projects
+      </li>
+      <li>
+        My <a rel="me" href="https://youtube.com/@JuxGD">main YouTube Channel</a>, where
+        I play GD and other games
+      </li>
+      <li>
+        My <a rel="me" href="https://youtube.com/@juxstep">secondary YouTube Channel</a>,
+        where I mess around with FL Studio and do other kinds of videos
+      </li>
+      <li>
+        My <a rel="me" href="https://catodon.social/@JuxGD">main Fediverse account</a>,
+        where you can follow and interact with me (<a
+          href="https://bsky.app/profile/JuxGD.catodon.social.ap.brid.gy"
+          >bridged to Bluesky</a
+        >
+        with <a href="https://fed.brid.gy">Bridgy Fed</a>) (<a
+          href="https://activitypub.rocks"
+          >What is the Fediverse?</a
+        >)
+      </li>
+      <li>
+        My <a rel="me" href="https://mastodon.social/@JuxGD">alt Fediverse account</a>,
+        idk it's kinda just there go follow (<a
+          href="https://bsky.app/profile/JuxGD.mastodon.social.ap.brid.gy"
+          >bridged to Bluesky</a
+        >
+        with <a href="https://fed.brid.gy">Bridgy Fed</a>)
+      </li>
+      <li>
+        My <a rel="me" href="https://bandwagon.fm/@Jux">Bandwagon</a> (Fediverse) and
+        <a rel="me" href="https://audius.co/JuxGD">Audius</a> where I post my music (<a
+          href="https://bsky.app/profile/Jux.bandwagon.fm.ap.brid.gy"
+          >Bandwagon bridged to Bluesky</a
+        >
+        with <a href="https://fed.brid.gy">Bridgy Fed</a>)
+      </li>
+      <li>
+        My <a rel="me" href="https://twitter.com/JReplicat72895">Twitter</a>, where I do
+        absolutely nothing, it's there just for convenience
+      </li>
+      <li>
+        <a rel="me" href="https://juxgd.github.io/music"
+          >Info on me as a music producer</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss"></style>
